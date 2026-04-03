ac3b2a7e75fa9d51666e56f4c27da29aa31d7829
David Ruiz Requejo <davidrr.22.12@gmail.com>
2026-04-03 17:07:40 +0200

    feat(db): add email and audit fields to users in data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,59 @@
-MERGE INTO users (username, password, role, enabled)
+MERGE INTO users (
+    username,
+    email,
+    password,
+    role,
+    enabled,
+    email_verified,
+    email_verified_at,
+    last_login_at,
+    created_at,
+    updated_at,
+    created_by,
+    updated_by
+    )
     KEY (username)
     VALUES (
     'admin',
+    'admin@test.com',
     '$2a$10$WYP5.LREBkTgamnp7/xTTeLrzaRBcdNHbjs4VJ0xk/4JB9eZrPIzG',
     'ADMIN',
-    TRUE
+    TRUE,
+    TRUE,
+    CURRENT_TIMESTAMP,
+    NULL,
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP,
+    1,
+    1
     );
 
-MERGE INTO users (username, password, role, enabled)
+MERGE INTO users (
+    username,
+    email,
+    password,
+    role,
+    enabled,
+    email_verified,
+    email_verified_at,
+    last_login_at,
+    created_at,
+    updated_at,
+    created_by,
+    updated_by
+    )
     KEY (username)
     VALUES (
     'user',
+    'user@test.com',
     '$2a$10$oORx.RofsB4UbrI1hjsb6uZWs42SQoRgFxQihd4DXcDeEecmTc.IK',
     'USER',
-    TRUE
+    TRUE,
+    TRUE,
+    CURRENT_TIMESTAMP,
+    NULL,
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP,
+    2,
+    2
     );
